72be84a35a58d872c8e8b6128733f60db058e306
Aryan Bhasein <aryanbhasein1@gmail.com>
2024-06-01 18:28:12 +0530

    Added homepage screen with 2 addtional custom widgets for section tile and circle avatar.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,130 @@
+import 'package:doctor_app/shared/widgets/titles/section_tile.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Create a bloc and provide it to the homeview
+    return const HomeView();
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Create the homeview UI
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome',
+              style: textTheme.bodyMedium,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Aryan B',
+              style: textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: colorScheme.secondary,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Pune, India',
+                  style: textTheme.bodySmall!
+                      .copyWith(color: colorScheme.secondary),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Icon(
+                  Icons.expand_more,
+                  color: colorScheme.secondary,
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_outlined),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(64),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Search for doctors',
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: Container(
+                  margin: const EdgeInsets.all(4),
+                  padding: const EdgeInsets.all(8),
+                  decoration:
+                      BoxDecoration(color: colorScheme.onSurfaceVariant),
+                  child: Icon(
+                    Icons.filter_alt_outlined,
+                    color: colorScheme.surfaceVariant,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            _DoctorCategories(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DoctorCategories extends StatelessWidget {
+  const _DoctorCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //Section title
+        SectionTitle(
+          title: 'Categories',
+          action: 'See all',
+          onPressed: () {},
+        )
+
+        //Icons
+      ],
+    );
+  }
+}
